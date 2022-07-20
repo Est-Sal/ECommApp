ecf66cfa097d1868c085ea211efeb2d385635c31
Esteban Salazar <estebans542@gmail.com>
2022-07-20 16:37:24 -0600

    hello

--- EcommerceApp/src/main/java/com/xadmin/usermanagement/dao/UserDao.java
+++ EcommerceApp/src/main/java/com/xadmin/usermanagement/dao/UserDao.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.xadmin.usermanagement.bean.User;
 
@@ -80,11 +82,54 @@ public class UserDao {
 	
 	
 	// Select all users
-	
+	public List<User> selectAllUsers() {
+		List<User> users = new ArrayList<>();
+		// Step 1: Establishing a connection
+		try (Connection connection = getConnection();
+				// Step 2: Create a statement using connection object
+				PreparedStatement preparedStatement = connection.prepareStatement (SELECT_ALL_USERS);) {
+			System.out.println(preparedStatement);
+			// Step 3: Execute the query or update query
+			ResultSet rs = preparedStatement.executeQuery();
+			
+			//Step 4: Process the ResultSet object
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				String country = rs.getString("country");
+				users.add(new User(id, name, email, country));
+			}
+		} catch (SQLException e) {
+			printSQLException(e);
+		}
+		return users;
+	}
 	// Update user
-	
+	public boolean updateUser(User user) throws SQLException {
+		boolean rowUpdated;
+		try (Connection connection = getConnection();
+				PreparedStatement statement = connection.prepareStatement(UPDATE_USERS_SQL);) {
+			System.out.println("updated User: "+statement);
+			statement.setString(1, user.getName());
+			statement.setString(2, user.getEmail());
+			statement.setString(3, user.getCountry());
+			statement.setInt(4, user.getId());
+			
+			rowUpdated = statement.executeUpdate() > 0;
+		}
+		return rowUpdated; 
+	}
 	// Delete user
-	
+	public boolean deleteUser(int id) throws SQLException {
+		boolean rowDeleted;
+		try (Connection connection = getConnection();
+				PreparedStatement statement = connection.prepareStatement(DELETE_USERS_SQL);) {
+			statement.setInt(1, id);
+			rowDeleted = statement.executeUpdate() > 0;
+		}
+		return rowDeleted;
+	}
 	
 	private void printSQLException(SQLException ex) {
 		for (Throwable e : ex) {
